5ecd02dec2c1ba362dddbb7c08f6ba64cfbd8f58
Karīna Meldere <meld.karina@gmail.com>
2026-08-11 11:49:20 +0300

    Finally implementing the tree idea

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import {ScrollTrigger, SplitText} from "gsap/all";
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 
 import Navigation from './components/Navigation'
@@ -7,12 +9,31 @@ import Playground from "./components/Playground.tsx";
 import Contacts from "./components/Contacts.tsx";
 import Footer from "./components/Footer.tsx";
 
-gsap.registerPlugin (ScrollTrigger, SplitText);
+gsap.registerPlugin(ScrollTrigger, SplitText, useGSAP);
 
 function App() {
+    const bgRef = useRef<HTMLDivElement>(null);
+
+    useGSAP(() => {
+        gsap.to(bgRef.current, {
+            yPercent: -23,
+            ease: "none",
+            scrollTrigger: {
+                trigger: document.body,
+                start: "top top",
+                end: "bottom bottom",
+                scrub: true,
+            },
+        });
+    });
 
     return (
-        <div className="flex flex-col">
+        <div className="relative flex flex-col">
+            <div
+                ref={bgRef}
+                className="fixed left-0 top-0 w-full h-[130%] -z-10 bg-cover bg-bottom will-change-transform"
+                style={{ backgroundImage: "url('/tree.jpg')" }}
+            />
             <Navigation />
             <Hero />
             <Playground />
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,6 @@
 const Hero = () => {
     return (
         <div className="flex flex-col items-center justify-center h-screen mb-20">
-            <img src="/grill.jpg" alt="hero" className=" w-full h-full object-cover opacity-50" />
             <div className="absolute text-center">
                 <h1 className="text-9xl pb-10">
                     Pie brāļa
