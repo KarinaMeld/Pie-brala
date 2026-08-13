7c7c86f4ea35c7793b75f1faa6d0d58348ef51c0
Karīna Meldere <meld.karina@gmail.com>
2026-08-13 12:06:12 +0300

    Styling changes

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,15 +1,13 @@
 const Menu = () => {
     return (
         <div className="section" id="menu">
-            <div className="grid grid-cols-2 gap-20 my-1/4">
-                <a className="relative block group">
-                    <img src="/food.jpg" alt="playground" className="menu-image" />
+            <div className="flex flex-row gap-20 w-full">
+                <a className="menu-link">
                     <h2 className="menu-title">
                         Ēdienkarte
                     </h2>
                 </a>
-                <a className="relative block group">
-                    <img src="/drinks.jpg" alt="playground" className="menu-image" />
+                <a className="menu-link">
                     <h2 className="menu-title">
                         Dzērienkarte
                     </h2>
